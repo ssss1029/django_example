823cae0bee6330a404b6ce48dbddec7bfa7666fb
ssss1029 <sauravkadavath@gmail.com>
2017-08-27 13:13:22 -0700

    going to do login stuff

--- login/views.py
+++ login/views.py
@@ -7,6 +7,16 @@ from django.template import loader
 
 # Create your views here.
 def index(request):
-	template = loader.get_template('login/index.html')
-	context = {}
-	return HttpResponse(template.render(context, request))
+	if request.method == "GET": 
+		# Give back the login page
+		template = loader.get_template('login/index.html')
+		context = {}
+		return HttpResponse(template.render(context, request))
+	elif request.method == "POST":
+		# Actually log the user object in
+		pass
+	else:
+		# There was a mistake
+		return HttpResponse("<h1> This endpoint does not support request type: " 
+			+ request.method 
+			+ " </h1>")
